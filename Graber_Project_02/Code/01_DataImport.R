49d15ed298a67cec84ae258fb7d994e1a913d72c
cgraber2 <71276576+cgraber2@users.noreply.github.com>
2020-09-14 12:17:34 -0600

    Add files via upload

---
+++ Graber_Project_02/Code/01_DataImport.R
@@ -0,0 +1,13 @@
+############################################################################## 
+### file: 01_DataImport       
+### authors: Connor Graber
+### creation date: 09/14/2020 
+### description: Import lead-iq-01.csv data 
+############################################################################## 
+
+setwd("/Users/cgraber/Graber_Project_02/DataRaw")
+
+lead_iq <- read.csv("lead-iq-01.csv")
+
+
+
